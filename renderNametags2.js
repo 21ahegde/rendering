2cc77a8182544b1926c7ac600c479cb8e68c6807
Aaron Hegde <hegde.aaron@gmail.com>
2020-03-07 10:19:54 -0500

    extra nametags

--- renderNametags2.js
+++ renderNametags2.js
@@ -2,9 +2,19 @@ function renderNametags(nametags) {
     var name = [];
     for (var i=0; i < nametags.length; i++) {
         var newNametag = `
-        <div class="text-center mt-5">
-            <div>${nametags[i]}</div>
+        <div class="card" style="width: 18rem;">
+       
+            <div class="card-body">
+                <h5 class="card-title" style="background:blue; color:white; text-align:center;">Hello My Name is:</h5>
+                    <p class="card-text"> 
+                        <div class="text-center mt-5">
+                         <div>${nametags[i]}</div>
+                        </div>
+                    </p>
+           
+            </div>
         </div>
+       
         
     `
     name.push(newNametag); 
